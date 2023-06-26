dfcdaa36531e070649e8edeb502845d976d23fad
Alexander Veselov <collabsis.g@gmail.com>
2023-06-26 22:17:51 +0400

    Completed Ch_1

---
+++ Chapter_1/exercise/exercise_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Read about RBenv - https://github.com/rbenv/rbenv"
+
+sudo dnf install ruby
+sudo dnf install rubygem-{tk{,-doc},rake,irb,test-unit}
+
+ruby -v
+
+echo "Read more https://developer.fedoraproject.org/tech/languages/ruby/ruby-installation.html"
---
+++ Chapter_1/exercise/exercise_2.rb
@@ -0,0 +1,1 @@
+puts 'Alexander Owls\' Catcher'
